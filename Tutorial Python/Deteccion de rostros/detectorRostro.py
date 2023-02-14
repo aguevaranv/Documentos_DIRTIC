2dfac46920c5d5711585f8247acffa84d908095b
aguevarac <aguevarac@armada.mil.ec>
2023-02-14 15:45:14 -0500

    deteccion de rostros

--- Tutorial Python/Deteccion de rostros/detectorRostro.py
+++ Tutorial Python/Deteccion de rostros/detectorRostro.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as numpy
 
+faceClassif = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 image = cv2.imread('oficina.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+faces = faceClassif.detectMultiScale(gray,
+	scaleFactor=1.1,
+	minNeighbors=5,
+	minSize=(70,70),
+	maxSize=(200,200))
+
+for (x,y,w,h) in faces:
+	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
 cv2.imshow('image',image)
-#cv2.waitkey(0)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
